0c2a9237084d626242eb4a4f8ae495b7363c2ea5
ahmednadeem9112 <ahmednadeem9112@gmail.com>
2020-08-11 00:42:26 +0500

    changes

--- index.js
+++ index.js
@@ -35,9 +35,90 @@ current = current.next;
 current.next = node;    
 }
 this.size++;
+}
+//get at index 
+
+getAt(index) {
+let current = this.head;
+let count = 0;
+while(current) {
+if (count == index) {
+
+    console.log(current.data);
+}
+count++;
+current = current.next;
+}
+return null;
+
+}
+//Remove at index
+removeAt(index) {
+if (index > 0 && index > this.size) {
+return;
+}
+let current = this.head;
+let previous;
+let count = 0;
+//remove the first 
+if (index === 0) {
+this.head = current.next;
+} else {
+    while(count < index) {
+count++;
+previous = current;
+current = current.next;
+
+    }
+previous.next = current.next;
+}
+this.size--;
 
 
 }
+
+//clear List 
+clearList() {
+this.head = null;
+this.size = 0;
+
+}
+
+
+
+
+
+
+//Insert At index 
+insertAt(data,index) {
+//if index is out of range
+if (index > 0 && index > this.size) {
+    return;
+}
+//for the first index
+if (index === 0) {
+    this.head = new Node(data,this.head);
+return;
+}
+const node = new Node(data);
+let current,previous;
+
+//set current to first
+current = this.head;
+let count = 0;
+
+while (count < index) {
+previous = current;
+count++;
+current = current.next;
+
+}
+
+node.next = current;
+previous.next = node;
+this.size++;
+}
+
 
 
 
@@ -52,13 +133,7 @@ current = current.next;
 
 }
 
-
 }
-
-
-
-
-
 
 }
 
@@ -66,11 +141,9 @@ const ll = new LinkedList();
 ll.insertFirst(100);
 ll.insertFirst(200);
 ll.insertFirst(300);
-ll.insertFirst(400);
+ll.insertLast(400);
+ll.clearList();
 ll.Printlistdata();
-
-
-
-
-
+ll.removeAt(2)
+//ll.getAt(3);
 
